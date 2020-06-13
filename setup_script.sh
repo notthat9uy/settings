03509bbd5058fc97e55f66506714a96fc933ba00
notthat9uy <notthat9uy>
2020-06-13 10:59:11 -0700

    Updates for python3

--- setup_script.sh
+++ setup_script.sh
@@ -27,20 +27,17 @@ cat .bashrc >> ~/.bashrc
 
 # pwntools
 yes Y | sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install --upgrade git+https://github.com/Gallopsled/pwntools.git@dev
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pwntools
 
 # gef
 wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
 echo source ~/.gdbinit-gef.py >> ~/.gdbinit
 
-yes Y | sudo apt-get install python3-pip 
-
-sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade keystone-engine
-sudo pip3 install --upgrade unicorn
-sudo pip3 install --upgrade capstone
-sudo pip3 install --upgrade ropper
+pip3 install --upgrade keystone-engine
+pip3 install --upgrade unicorn
+pip3 install --upgrade capstone
+pip3 install --upgrade ropper
 
 # tmux
 # Don't forget to do prefix + I to install all plugins!
